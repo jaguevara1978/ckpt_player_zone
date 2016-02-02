43fb3ee7782a8b403c92693be7469e4ebda6225b
Alex Guevara <aguevara@matoot.com>
2016-02-02 10:05:26 -0800

    Trivia game controller connecting to the new WebServices and new UX

--- app/trivia/trivia.js
+++ app/trivia/trivia.js
@@ -11,14 +11,16 @@
 angular.module( 'app.trivia' ).controller( 'Trivia', Trivia );
 
 /*@ngInject*/
-function Trivia( ApiService, CelebrationService, SorryService, $location, $rootScope, FlashService, $scope, $timeout, $sce ) {
+function Trivia( ApiService, CelebrationService, SorryService, $location, $rootScope, FlashService, $scope, $timeout, $interval, $sce ) {
     var vm = this;
     vm.validateAnswer = validateAnswer;
     vm.getQuestion = getQuestion;
     vm.trustSrc = trustSrc;
     vm.goToRewards = goToRewards;
+    vm.questionOptionClass = questionOptionClass;
 
-    getQuestion( );
+    getQuestion();
+
 /*
 // Main
 initHeader( );
@@ -30,47 +32,121 @@ initAnimation( );
     }
 
     function getQuestion( ) {
+        vm.showReward = false;
         vm.feedback = false;
+
         ApiService.get( 'challenge', 0 )
             .then( function( response ) {
                 if ( response.success ) {
+                    try {
+                        vm.data.correct_answer_id = null;
+                        vm.data.player_answer_id = null;
+                        countDownClass();
+                   } catch( error ) { }
                     vm.data = response.data;
+
+
+                    vm.data.actual_time = vm.data.display_time;
+                    vm.countdown_promise = $interval( function( ) {
+                         vm.data.actual_time--;
+                         if ( vm.data.actual_time == 0 ) {
+                            $interval.cancel( vm.countdown_promise );
+                         }
+                    }, 1000 );
+
+                    vm.timeout_promise = $timeout( function() {
+                        validateAnswer(-1);
+                    }, vm.data.display_time * 1000)
 
                     console.log( 'Get Question:' );
                     console.log(vm.data);
+                   
                 } else {
                     FlashService.Error( response.message );
                 }
             }
         );
+
+
+    }
+
+    function questionOptionClass( value ) {
+        var text = '';
+        if ( vm.data.correct_answer_id == value)  {
+            text = "btn-trivia btn-correct";
+        } else if (vm.data.correct_answer_id != value && vm.data.player_answer_id == value) {
+            text = "btn-trivia btn-wrong";
+        } else if (vm.data.correct_answer_id != value ) {
+            text = "btn-trivia";
+        }
+        return text;
     }
 
     function validateAnswer( option ) {
         //vm.aside.option = option;
+        $interval.cancel( vm.countdown_promise );
+        $timeout.cancel( vm.timeout_promise );
         var data = {
             tr_id: option
         }        
         ApiService.post( 'challenge_process', data )
             .then( function( response ) {
                 if ( response.success ) {
-                    vm.data = response.data;
-                   
+                    vm.data = MergeRecursive( vm.data, response.data );
+/*
                     console.log( 'After Validation:' );
                     console.log(vm.data);
-
-                    // To show hide Feedback object
-                    vm.feedback = true;
+*/
+                    vm.feedback = false;
+                    
                     $timeout(function() {
-                        vm.feedback = false;
+                        //To show hide Feedback object
+                        vm.feedback = true;
+
+                        // If there is a reward then I will show the reward object
+                        if (vm.data.reward !== null) {
+                            $timeout(function() {
+                                // After 2 seconds no more feedback and show reward
+                                vm.feedback = false;
+                                vm.showReward = true;
+                            }, 2000);
+                        } else {}
+
                     }, 2000);
+                    console.log(vm.feedback);
                 } else {
-                    console.log(response);
-                    FlashService.Error( response.message );
+                   console.log(response);
+                   FlashService.Error( response.message );
                 }
             }
         );
     }
+
+    /*
+    * Recursively merge properties of two objects 
+    */
+    function MergeRecursive(obj1, obj2) {
     
+      for (var p in obj2) {
+        try {
+          // Property in destination object set; update its value.
+          if ( obj2[p].constructor==Object ) {
+            obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+    
+          } else {
+            obj1[p] = obj2[p];
+    
+          }
+    
+        } catch(e) {
+          // Property in destination object not set; create it and set its value.
+          obj1[p] = obj2[p];
+    
+        }
+      }
+      return obj1;
+    }
+
     function goToRewards() {
         $location.path( '/rewards' );
     }
